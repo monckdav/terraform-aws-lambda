abfd392e7a6b5261d5821863149d9cfcefb08b0b
David Moncka <david.moncka@telia.no>
2019-04-16 15:14:37 +0200

    added source_code_hash parameter

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ resource "aws_lambda_function" "main" {
   timeout                        = "${var.timeout}"
   role                           = "${aws_iam_role.main.arn}"
   reserved_concurrent_executions = "${var.reserved_concurrent_executions}"
+  source_code_hash               = "${var.source_code_hash}"
 
   environment {
     variables = "${var.environment}"
@@ -33,6 +34,7 @@ resource "aws_lambda_function" "vpc" {
   timeout                        = "${var.timeout}"
   role                           = "${aws_iam_role.main.arn}"
   reserved_concurrent_executions = "${var.reserved_concurrent_executions}"
+  source_code_hash               = "${var.source_code_hash}"
 
   vpc_config {
     subnet_ids         = ["${var.subnet_ids}"]
@@ -59,6 +61,7 @@ resource "aws_lambda_function" "main_s3" {
   timeout                        = "${var.timeout}"
   role                           = "${aws_iam_role.main.arn}"
   reserved_concurrent_executions = "${var.reserved_concurrent_executions}"
+  source_code_hash               = "${var.source_code_hash}"
 
   environment {
     variables = "${var.environment}"
@@ -85,6 +88,7 @@ resource "aws_lambda_function" "vpc_s3" {
   timeout                        = "${var.timeout}"
   role                           = "${aws_iam_role.main.arn}"
   reserved_concurrent_executions = "${var.reserved_concurrent_executions}"
+  source_code_hash               = "${var.source_code_hash}"
 
   vpc_config {
     subnet_ids         = ["${var.subnet_ids}"]
